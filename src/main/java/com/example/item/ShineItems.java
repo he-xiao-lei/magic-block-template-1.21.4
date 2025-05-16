7db3cb304bcf4b6d3fb9b8cfd1e3afe00abe317d
vblaws <h3239498031@gmail.com>
2025-05-16 13:58:13 +0800

    delete file

--- src/main/java/com/example/item/ShineItems.java
+++
@@ -1,9 +0,0 @@
-package com.example.item;
-
-import net.minecraft.item.Item;
-
-import java.util.function.Function;
-
-public class ShineItems {
-    private static final Item SHINE_ITEM = register("shine_item", Function<>)
-}
